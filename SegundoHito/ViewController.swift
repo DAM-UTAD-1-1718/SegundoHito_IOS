05a1700734207816b249d09d0a39c6a9f3b9b9d4
Miguel Martinez <miguel1603mg@gmail.com>
2018-04-10 12:56:57 +0200

    Initial Commit

---
+++ SegundoHito/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SegundoHito
+//
+//  Created by Miguel Angel Martinez Gonzalez on 10/4/18.
+//  Copyright © 2018 Miguel Angel Martinez Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
